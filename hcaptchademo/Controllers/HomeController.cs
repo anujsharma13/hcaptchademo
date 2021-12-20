4cc525a9006a6ac9c8c69caa2e3bbb7c85def4db
AnSharma <AnSharma@LAP-55379>
2021-12-20 16:33:51 +0530

    Add project files.

---
+++ hcaptchademo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using hcaptchademo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace hcaptchademo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Result(Student s)
+        {
+            //var _googlerecaptcha=GoogleCaptchaService.verifyrecaptcha()
+            string EncodedResponse = Request.Form["g-Recaptcha-Response"];
+            GoogleCaptchaService googleCaptchaService = new GoogleCaptchaService();
+            var _googlecaptcha = await googleCaptchaService.verifycaptcha(EncodedResponse);
+            if (_googlecaptcha)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            return View("Result", s);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ hcaptchademo/Models/GoogleCaptchaService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace hcaptchademo.Models
+{
+    public class GoogleCaptchaService
+    {
+        public async Task<bool> verifycaptcha(string _Token)
+        {
+            
+            using(var client = new HttpClient())
+            {
+              var values=new Dictionary<string, string>
+                {
+                  { "response" , _Token },
+                  { "secret" , "0x0000000000000000000000000000000000000000" }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var verify = await client.PostAsync("https://hcaptcha.com/siteverify", content);
+                var captcharesponsejson=await verify.Content.ReadAsStringAsync();
+                var captcharesult=JsonConvert.DeserializeObject<GoogleCaptchaRespo>(captcharesponsejson);
+                return captcharesult.success;
+            }
+        }
+    }
+
+    public class GoogleCaptchaData
+    {
+        public string response { get; set; }
+        public string secret { get; set; }
+    }
+    public class GoogleCaptchaRespo
+    {
+        public bool success { get; set; }
+        public double score { get; set; }
+        public string action { get; set; }
+        public DateTime challenge_ts { get; set; }
+        public string hostname { get; set; }
+    }
+}
